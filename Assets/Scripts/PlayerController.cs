ff463b8f532cb008cbbfb06b4cfcdc7ea2807c82
Zander Goodwin <zanderngoodwin@gmail.com>
2025-11-17 12:20:44 -0400

    Revamped player movement, player does not jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,42 @@
-using Unity.Hierarchy;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed;
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool groundedPlayer;
+    public float speed;
+    public float jumpHeight;
+    public float gravity;
     
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
     // Update is called once per frame
     void Update()
     {
-        //Get player movement
-        float moveX = Input.GetAxis("Horizontal");
-        float moveZ = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveX, 0f, moveZ);
-        
-        movement.Normalize(); //Normalize vector
+        groundedPlayer = controller.isGrounded;
 
-        transform.Translate(movement * moveSpeed * Time.deltaTime); //Frame independant
+        if(groundedPlayer && velocity.y > 0) //Check if player is on the ground
+        {
+            velocity.y = 0;
+        }
+
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), velocity.y, Input.GetAxis("Vertical")); //Gets player input
+        if (move != Vector3.zero) //If input detected, move player
+        {
+            gameObject.transform.forward = move;
+        }
+
+        if (Input.GetButtonDown("Jump") && groundedPlayer) //Gets jump input and checks if player is already jumping
+        {
+            velocity.y += Mathf.Sqrt(jumpHeight * -2f * gravity); //Sqrt square roots jumpHeight * -2f * gravity
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(move * Time.deltaTime * speed); //Frame independant
     }
 }
